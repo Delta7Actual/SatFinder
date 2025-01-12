0883a8fb0019e15aa4a984802d6cb3f75a0c0376
Delta7Actual <142044822+Delta7Actual@users.noreply.github.com>
2025-01-12 23:55:38 +0200

    bit more work. weird bug when opening through Emulator.

--- app/src/main/java/com/example/satfinder/Fragments/DetailsFragment.java
+++ app/src/main/java/com/example/satfinder/Fragments/DetailsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,10 +16,14 @@ public class DetailsFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private TextView tvSatelliteName;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+        tvSatelliteName = view.findViewById(R.id.tv_satellite_name);
+        return view;
     }
 }
--- app/src/main/java/com/example/satfinder/Fragments/SearchFragment.java
+++ app/src/main/java/com/example/satfinder/Fragments/SearchFragment.java
@@ -6,11 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.satfinder.Managers.SatelliteManager;
+import com.example.satfinder.Objects.Interfaces.IN2YOCallback;
+import com.example.satfinder.Objects.Interfaces.ISatelliteResponse;
+import com.example.satfinder.Objects.SatelliteTLEResponse;
 import com.example.satfinder.R;
 
 public class SearchFragment extends Fragment {
@@ -25,7 +29,6 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
         etSatelliteId = view.findViewById(R.id.et_satellite_id);
@@ -43,7 +46,23 @@ public class SearchFragment extends Fragment {
         }
         SatelliteManager manager = SatelliteManager.getInstance();
         int satelliteId = Integer.parseInt(etSatelliteId.getText().toString());
+        manager.fetchSatelliteTLE(satelliteId, new IN2YOCallback() {
 
-        // TODO: CONTINUE THIS SHIT
+            @Override
+            public void onSuccess(ISatelliteResponse response) {
+                SatelliteTLEResponse tleResponse = (SatelliteTLEResponse) response;
+                TextView tvSatelliteName = requireActivity()
+                        .getSupportFragmentManager()
+                        .findFragmentById(R.id.fragment_container)
+                        .getView()
+                        .findViewById(R.id.tv_satellite_name);
+                tvSatelliteName.setText(tleResponse.getInfo().getSatname());
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                Toast.makeText(SearchFragment.this.getContext(), "Couldn't fetch data. Make sure you provided a valid ID", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
